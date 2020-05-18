e2e39fca2f826e1917fae406c045bd7ea3935d0f
et <et@qq.com>
2020-05-18 15:50:22 +0800

    提交Spring-ioc-review

---
+++ spring-ioc-review/src/main/java/com/etoak/config/UserConfig.java
@@ -0,0 +1,25 @@
+package com.etoak.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.etoak.action.UserAction;
+import com.etoak.service.UserService;
+
+@Configuration
+public class UserConfig {
+	
+	@Bean("userService")
+	public UserService userService() {
+		return new UserService();
+	}
+	
+	@Bean
+	public UserAction userACtion(@Qualifier("userService")
+	UserService userService) {
+		UserAction userAction =new UserAction();
+		userAction.setUserService(userService);
+		return userAction;
+	}
+}
